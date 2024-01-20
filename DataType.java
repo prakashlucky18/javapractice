623138a7654cc5dcace7b1bc48e483e87c1853cd
prakashlucky18 <95863956+prakashlucky18@users.noreply.github.com>
2024-01-20 15:49:56 +0530

    data types in java

---
+++ DataType.java
@@ -0,0 +1,22 @@
+public class DataType
+{
+    public static void main(String[] args) {
+        int num=2;
+        byte b=127;
+        short s=557;
+        long l=54678l;
+        float f=7.8f;
+        double d=5.8;
+        char c='p';
+        boolean bool=true;
+        System.out.println(num);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
+
+    }
+}
