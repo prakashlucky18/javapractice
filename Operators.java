e070ab7da358dc12071420dea521c47bc7232594
prakashlucky18 <95863956+prakashlucky18@users.noreply.github.com>
2024-01-21 15:14:50 +0530

    operators in java

---
+++ Operators.java
@@ -0,0 +1,31 @@
+package corejava;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=20;      /* arithmetic operators */
+	       int b=10;
+	       int c=a+b;
+	       int d=a-b;
+	       int e=a*b;
+	       int f=a/b;
+	       int g=a%b;
+	       int h=++a;
+	       int i=--b;
+	        System.out.println(c);
+	        System.out.println(d);
+	        System.out.println(e);
+	        System.out.println(f);
+	        System.out.println(g);
+	        System.out.println(h);
+	        System.out.println(i);
+	        int x=10;
+	        x=x+3;
+	        System.out.println(x);
+	         int y=x-3;
+	         System.out.println(y);
+	        
+	        
+	}
+
+}
