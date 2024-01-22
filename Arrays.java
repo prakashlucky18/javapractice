1855b717e81b6c0b1fde3264f7ccb27e391315be
prakashlucky18 <95863956+prakashlucky18@users.noreply.github.com>
2024-01-22 16:03:24 +0530

    array creation in java
    array are used to store multi[ple values in a single variable instead of declaring separate variables for each value.

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+package corejava;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+//		array creation
+		int num[]= {2,4,5};
+		System.out.println(num[0]);
+		
+//		another syntax for creating array
+		int mynum[]=new int[4];
+		mynum[0]=7;
+		mynum[1]=1;
+		mynum[2]=10;
+		mynum[3]=9;
+		for(int i=0;i<4;i++)
+		System.out.println(mynum[i]);
+		
+	}
+
+}
