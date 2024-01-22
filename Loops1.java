63164440de232c18937a69a46ec300891794c308
prakashlucky18 <95863956+prakashlucky18@users.noreply.github.com>
2024-01-22 10:52:58 +0530

    for loop,while loop,do-while loop in java

---
+++ Loops1.java
@@ -0,0 +1,27 @@
+package corejava;
+
+public class Loops1 {
+
+	public static void main(String[] args) {
+		int i=0;
+//		while loop
+		while(i<5)
+		{
+			System.out.println("hi" +i);
+			i++;
+		}
+		int j=0;
+//		do while loop
+		do
+		{
+			System.out.println("how are you" +j);
+			j++;
+		}while(j<5);
+//		for loop
+		for(int x=0;x<5;x++)
+		{
+			System.out.println(x);
+		}
+	}
+
+}
