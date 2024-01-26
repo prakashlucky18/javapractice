e6865812f1ad8d9bbf0bce6283c1e08053f59a14
prakashlucky18 <95863956+prakashlucky18@users.noreply.github.com>
2024-01-26 17:09:30 +0530

    Accessing the non static members in static method

--- StaticVariables.java
+++ StaticVariables.java
@@ -3,11 +3,17 @@ class Mobile
 {
 	String model;
 	int price;
+//	Static variables shared among all the objects
 	 static String name;
 	
 	public void show()
 	{
 		System.out.println(model+" : "+price+" : "+name);
+	}
+	public static void show1(Mobile obj1)
+	{
+//		Accessing the non static members
+		System.out.println(obj1.model+" : "+obj1.price+" : "+name);
 	}
 }
 
@@ -17,12 +23,16 @@ public class StaticVariables {
 		Mobile obj1=new Mobile();
 		obj1.model="apple";
 		obj1.price=100000;
+//		shared for both obj1 and obj2
 		Mobile.name="phones";
 		Mobile obj2=new Mobile();
 		obj2.model="realme";
 		obj2.price=20000;
+		
 		obj1.show();
 		obj2.show();
+//		acccessing the non static members
+		Mobile.show1(obj1);
 
 	}
 
